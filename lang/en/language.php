8957ca81304ca99ee9484982ff371769133f6b26
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:38:04 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,8 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/counter/module.php
+
+$LANG['This page has been viewed <span class="notranslate">{HITS}</span> times. '] = 'This page has been viewed <span class="notranslate">{HITS}</span> times. ';
+$LANG['First hit: <span class="notranslate">{FIRST_HIT}</span>'] = 'First hit: <span class="notranslate">{FIRST_HIT}</span>';
