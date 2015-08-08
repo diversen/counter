eb7cc0e3a403aca3a24d7dcb59726ef470c36c9b
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:20:35 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,6 @@
+<?php
+
+$LANG = array();
+
+$LANG['This page has been viewed <span class="notranslate">{HITS}</span> times. '] = 'Denne side er besøgt <span class="notranslate">{HITS}</span> gange.';
+$LANG['First hit: <span class="notranslate">{FIRST_HIT}</span>'] = 'Første hit: <span class="notranslate">{FIRST_HIT}</span>';
